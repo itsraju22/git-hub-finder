b0c6550b59a8636901bd4bceba7f29a70459bef8
Rajuangadi <raju.a@ensarsolutions.com>
2024-02-12 11:08:38 +0530

    initial commit

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import Socials from "../components/Socials";
+
+const Footer = () => {
+    return (
+        <div className="footer text-center">
+            <Socials />
+            <p>Developed and Maintained by Raju Angadi &copy; 2024</p>
+        </div>
+    )
+}
+
+export default Footer;
